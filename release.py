0bb4d748819c75b65e0cfa4b1d5f4e44c3b11b01
Jonas Jurczok <jonasjurczok@gmail.com>
2017-09-22 11:19:48 +0200

    added release script

---
+++ release.py
@@ -0,0 +1,48 @@
+#! python
+
+import os, sys, shutil, glob, zipfile
+
+version = ""
+
+with open("info.json") as info:
+    for line in info:
+        if ("\"version\"" in line):
+            contents = line.split("\"")
+            version = contents[3]
+            print "Found version " + version + "."
+
+if (version == ""):
+    print "No version found. Aborting"
+    sys.exit(-1)
+
+directory = "Todo-List_" + version
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+    print "Directory " + directory + " created."
+else:
+    print "Directory " + directory + " already exists. Aborting..."
+    sys.exit(-2)
+
+for file in glob.glob(r'*.lua'):
+    print "Copying " + file + "..."
+    shutil.copy(file, directory)
+
+print "Copying locales"
+shutil.copytree("locale", directory + "/locale")
+
+print "Copying info.json"
+shutil.copy("info.json", directory)
+
+print "Creating zipfile..."
+zipf = zipfile.ZipFile(directory + '.zip', 'w', zipfile.ZIP_DEFLATED)
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        zipf.write(os.path.join(root, file))
+zipf.close()
+
+print "Removing directory..."
+shutil.rmtree(directory)
+
+print "Release " + version + " completed."
