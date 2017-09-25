271a9d43a6c03fb928dab5dfe8fa26c50e2bb890
Jonas Jurczok <jjurczok@notebooksbilliger.de>
2017-09-25 21:28:32 +0200

    added first try of mock

--- spec/UI_spec.lua
+++ spec/UI_spec.lua
@@ -17,11 +17,22 @@ describe("UI tests", function()
     it("Editing a completed task should work", function()
         -- create task
 	local task = {["task"] = "test", ["assignee"] = nil}        
-        table.insert(global.todo.open, task)
-
-	-- mark complete
-        todo.mark_complete(1)        
+        table.insert(global.todo.done, task)
+      
         -- edit
+	local player = {}
+	player.gui = {}
+	player.gui.center = {}
+	player.gui.center.add = function(element)
+		element.add = function(child)
+			table.insert(element, child)
+			return child
+		end
+		table.insert(player.gui.center, element)
+		return element
+	end
+
+        todo.edit_task(player , 1)
         -- save
         -- verify changes
     end)
