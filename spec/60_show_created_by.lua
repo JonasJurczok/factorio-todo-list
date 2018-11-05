ae513d3fe28b612d7a2043534006bf241ca53d6e
Tarrke <tarrke@gmail.com>
2018-11-05 13:16:08 +0100

    fixed errors

--- spec/60_show_created_by.lua
+++ spec/60_show_created_by.lua
@@ -20,12 +20,13 @@ feature("#60 Show created/updated by", function()
 
         todo.create_add_edit_frame(player, task)
         local frame = todo.get_add_edit_frame(player)
-
-        assert(frame["todo_add_task_table"]["todo_created_by_playername"] == player.name)
         local created_by = frame["todo_add_task_table"]["todo_created_by_playername"]
-        faketorio.log.info("Creator: ", {created_by})
-        assert(frame["todo_add_task_table"]["todo_updated_by_playername"] == player.name)
         local updated_by = frame["todo_add_task_table"]["todo_updated_by_playername"]
+        
+        assert(created_by == player.name)
+        faketorio.log.info("Creator: ", {created_by})
+
+        assert(todo_updated_by == player.name)
         faketorio.log.info("Last editor: ", {updated_by})
     end)
 
@@ -43,9 +44,9 @@ feature("#60 Show created/updated by", function()
         assert(frame["todo_add_task_table"]["todo_created_by_playername"] == player1.name)
 
         -- Player2 update the task
-        todo.update(frame["todo_add_task_table"]["todo_created_by_playername"], 1, player2)
+        todo.update(frame["todo_add_task_table"]["todo_updated_by_playername"], 1, player2)
         todo.create_add_edit_frame(player, task)
         frame = todo.get_add_edit_frame(player)
-        assert(frame["todo_add_task_table"]["todo_created_by_playername"] == player2.name)
+        assert(frame["todo_add_task_table"]["todo_updated_by_playername"] == player2.name)
     end)
 )
