b6bbc88ac24d2f8dbe517843a551deb73a9dd123
davidwhite99 <davidawhite13@gmail.com>
2025-05-14 12:41:30 -0400

    Added check for subtask focusing
    Tested, functionality still works, covers possible edge cases where the name changes between creating the subtask and rebuilding the task gui.

--- src/todo/todo.lua
+++ src/todo/todo.lua
@@ -217,7 +217,9 @@ function todo.on_gui_confirmed(event)
         local id = todo.get_task_id_from_element_name(element.name, "todo_main_subtask_new_text_")
         todo.on_save_new_subtask_click(player, id)
         if (player.opened.valid) then
-            player.opened.todo_scroll_pane.todo_task_table[name].focus()
+            if (player.opened.todo_scroll_pane.todo_task_table[name]) then
+                player.opened.todo_scroll_pane.todo_task_table[name].focus()
+            end
         end
     end
 end
